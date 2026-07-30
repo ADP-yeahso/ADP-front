c51b3f434bdbafe025d218a36c5077a622c7d355
kkkkangji <jimin0604055@gmail.com>
2026-07-30 16:32:04 +0900

    feat: add gallery screen

---
+++ lib/screens/gallery/gallery_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class GalleryScreen extends StatelessWidget {
+  const GalleryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: AppBar(
+        title: Text('갤러리'),
+        automaticallyImplyLeading: false,
+      ),
+      body: Center(
+        child: Text('갤러리 화면'),
+      ),
+    );
+  }
+}

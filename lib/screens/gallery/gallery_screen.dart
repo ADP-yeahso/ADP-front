e2fac1bbad13b4dc97517f4c2db542b4f8a93e16
kkkkangji <jimin0604055@gmail.com>
2026-07-31 01:47:26 +0900

    feat: connect gallery screen to bottom navigation

--- lib/screens/gallery/gallery_screen.dart
+++ lib/screens/gallery/gallery_screen.dart
@@ -5,7 +5,7 @@ class GalleryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text('갤러리'),
         automaticallyImplyLeading: false,
